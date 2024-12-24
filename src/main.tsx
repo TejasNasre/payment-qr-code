ded37236c3673b58ed50fd42d754e39279794f79
Tejas Nasre <tejasnasre120@gmail.com>
2024-12-24 12:58:16 +0530

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,26 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { registerSW } from 'virtual:pwa-register';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+import { Analytics } from "@vercel/analytics/react";
 
 // Register service worker
 registerSW({
   onNeedRefresh() {
     // You can show a notification to the user that there's an update available
-    if (confirm('New content available. Reload?')) {
+    if (confirm("New content available. Reload?")) {
       window.location.reload();
     }
   },
   onOfflineReady() {
-    console.log('App ready to work offline');
+    console.log("App ready to work offline");
   },
 });
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 );
